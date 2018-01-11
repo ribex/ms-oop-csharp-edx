cccfbe3b0e40a7d8d8b08090b0ba2ebbcd9ab7db
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-10 21:05:55 -0500

    Create DrinksMachine class, add member variables, methods, implementation, and instantiate object

---
+++ MVA_Class_Demo/MVA_Class_Demo/DrinksMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVA_Class_Demo
+{
+    class DrinksMachine
+    {
+        // The following statements declare private member variables
+        public string _location;
+        public string _make;
+        public string _model;
+
+        // The following statements declare public methods
+        public void MakeCappuccino()
+        {
+            Console.WriteLine("Cappuccino is made.");
+        }
+        public void MakeEspresso()
+        {
+            // Method logic goes here
+        }
+    }
+}
---
+++ MVA_Class_Demo/MVA_Class_Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace MVA_Class_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine();
+            myMachine._location = "Kitchen";
+            myMachine._model = "DM1000";
+
+            Console.WriteLine(myMachine._location);
+            myMachine.MakeCappuccino();
+        }
+    }
+}
