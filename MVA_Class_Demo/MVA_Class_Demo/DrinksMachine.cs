f5b98fc9d973183e7939d224a56183c81bb31b00
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-11 21:51:49 -0500

    Encapsulate properties

--- MVA_Class_Demo/MVA_Class_Demo/DrinksMachine.cs
+++ MVA_Class_Demo/MVA_Class_Demo/DrinksMachine.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MVA_Class_Demo
 {
     class DrinksMachine
     {
+        public DrinksMachine(string loc, string make, string model)
+        {
+            this.Location = loc;
+            this.Make = make;
+            this.Model = model;
+        }
+
         // The following statements declare private member variables
-        public string _location;
-        public string _make;
-        public string _model;
+        private string _location;
+        private string _make;
+        private string _model;
+
+        public string Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
 
         // The following statements declare public methods
         public void MakeCappuccino()
