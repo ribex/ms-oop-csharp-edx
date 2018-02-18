d31c216f3a2b8acb92069dafc039a96569afb8d5
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-02-17 23:24:37 -0500

    Stub out classes

---
+++ Module1SelfAssessmentLab/Module1SelfAssessmentLab/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1SelfAssessmentLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public static int studentCount = 0;
+
+        public Student()
+        {
+            studentCount++;
+        }
+
+        public Student(string name)
+        {
+            Name = name;
+            studentCount++;
+        }
+    }
+
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public static int teacherCount = 0;
+
+        public Teacher()
+        {
+            teacherCount++;
+        }
+
+        public Teacher(string name)
+        {
+            Name = name;
+            teacherCount++;
+        }
+    }
+
+    public class UProgram
+    {
+    }
+
+    public class Degree
+    {
+    }
+
+
+    public class Course
+    {
+    }
+}
