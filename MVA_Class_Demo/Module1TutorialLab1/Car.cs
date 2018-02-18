e910f94fdc30fe75bed829a0d64653614b6db1a9
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-02-17 20:20:20 -0500

    Complete Lab1

---
+++ MVA_Class_Demo/Module1TutorialLab1/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace Module1TutorialLab1
+{
+    partial class Program
+    {
+        public class Car
+        {
+            public string Color { get; set; }
+            public int Year { get; set; }
+            public int Mileage { get; set; }
+        }
+    }
+}
---
+++ MVA_Class_Demo/Module1TutorialLab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1TutorialLab1
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var Car1 = new Car();
+
+            Car1.Color = "Cool Grey Khaki";
+            Car1.Year = 2018;
+            Car1.Mileage = 5;
+
+            Console.WriteLine($"This car is painted {Car1.Color}, was built in {Car1.Year}, and has {Car1.Mileage} miles on it.");
+        }
+    }
+}
