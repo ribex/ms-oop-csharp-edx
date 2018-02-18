1556c1cb28ce19e7c4c4d558d0ba50cd0eac3368
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-02-17 21:20:51 -0500

    Complete lab3

---
+++ Module1TutorialLab3/Module1TutorialLab3/Car.cs
@@ -0,0 +1,35 @@
+﻿namespace Module1TutorialLab3
+{
+    public class Car
+    {
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        private static int instances = 0;
+
+        public Car(string color, int year)
+        {
+            this.Color = color;
+            this.Year = year;
+            instances++;
+        }
+
+        public Car(int year, int mileage)
+        {
+            this.Year = year;
+            this.Mileage = mileage;
+            instances++;
+        }
+
+        public Car()
+        {
+            instances++; // increment each time a new Car object is constructed
+        }
+
+        public static int CountCars()
+        {
+            return instances;
+        }
+    }
+}
---
+++ Module1TutorialLab3/Module1TutorialLab3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1TutorialLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Car1 = new Car();
+            Car1.Color = "Cool Grey Khaki";
+            Car1.Year = 2018;
+            Car1.Mileage = 5;
+
+            var Car2 = new Car("Red", 2008);
+
+            int carCount = Car.CountCars();
+
+            Console.WriteLine($"There are {carCount} cars on inventory right now.");
+        }
+    }
+}
