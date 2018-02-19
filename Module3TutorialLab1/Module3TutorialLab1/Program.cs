f3b341a4ace3ad5624faf940c43b9c32c4f631ab
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-02-19 18:30:31 -0500

    Complete mod3 tutorial lab 1

---
+++ Module3TutorialLab1/Module3TutorialLab1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3TutorialLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReaderObject = null;
+
+            try
+            {
+                streamReaderObject = new StreamReader("file1.txt");
+
+                String contents = streamReaderObject.ReadToEnd();
+
+
+                Console.WriteLine("The file has {0} text elements.", new StringInfo(contents).LengthInTextElements);
+            }
+
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file cannot be found.");
+            }
+
+            finally
+            {
+                if (streamReaderObject != null)
+                {
+                    streamReaderObject.Close();
+
+                    streamReaderObject.Dispose();
+                }
+            }
+        }
+    }
+}
